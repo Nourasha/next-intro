da4c80d376db404a8943a000534c1b3624d07fd1
Nourasha <69981085+Nourasha@users.noreply.github.com>
2023-11-05 02:35:23 +0100

    //

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,9 @@ export default function NavBar() {
     <nav className="flex h-screen justify-center items-center">
       <Link
         href="/new"
-        className="border-none font-bold text-white opacity-50 hover:opacity-100 rounded-md shadow-2xl shadow-cyan-500/50 bg-cyan-500 text-xl p-3"
+        className="border-none font-bold text-white opacity-60 hover:opacity-100 rounded-md shadow-2xl shadow-cyan-500/50 bg-cyan-500 text-xl p-3"
       >
-        Produktene herfra
+        Trykk her
       </Link>
     </nav>
   );
